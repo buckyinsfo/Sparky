fde087afb7c26c3dd7d5a5c63ed6e15e2b78521c
buckyinsfo <buckyinsfo@gmail.com>
2019-01-08 00:48:45 -0800

    Add matrix class

--- Sparky-core/src/maths/maths.h
+++ Sparky-core/src/maths/maths.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 #include "vec2.h"
 #include "vec3.h"
 #include "vec4.h"
+#include "mat4.h"
+
+
+inline float toRadians(float degrees)
+{
+	return 2.0f * degrees * ((float)M_PI / 360.0f);
+}
