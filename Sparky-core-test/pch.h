72b0837f0fad418717ffc2b012496b3cbe2c1173
buckyinsfo <buckyinsfo@gmail.com>
2019-01-09 12:27:15 -0800

    Add gtest project and framework. TODO: convert sparky-core to a lib.

---
+++ Sparky-core-test/pch.h
@@ -0,0 +1,12 @@
+//
+// pch.h
+// Header for standard system include files.
+//
+
+#pragma once
+
+//#include <GLFW/glfw3.h>
+
+#include "gtest/gtest.h"
+
+#include "../Sparky-core/src/maths/maths.h"
