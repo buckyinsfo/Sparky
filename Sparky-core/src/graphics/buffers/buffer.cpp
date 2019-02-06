06aaacf0ac3ac94433e58f33abcd5ded17ba78c8
buckyinsfo <buckyinsfo@gmail.com>
2019-02-06 11:11:23 -0800

    Add buffer management classes.

--- Sparky-core/src/graphics/buffers/buffer.cpp
+++ Sparky-core/src/graphics/buffers/buffer.cpp
@@ -1,25 +1,27 @@
 #include "buffer.h"
 
 namespace sparky {
-	namespace mock {
+    namespace graphics {
+        namespace buffers {
 
-		Buffer::Buffer(GLsizei count, GLfloat* data, GLuint componentCount) :
-			m_componentCount(componentCount)
-{
-			glGenBuffers(1, &m_bufferID);
-			glBindBuffer(GL_ARRAY_BUFFER, m_bufferID);
-			glBufferData(GL_ARRAY_BUFFER, count * sizeof(GLfloat), data, GL_STATIC_DRAW);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
+            Buffer::Buffer(GLsizei count, GLfloat* data, GLuint componentCount) :
+                m_componentCount(componentCount)
+            {
+                glGenBuffers(1, &m_bufferID);
+                glBindBuffer(GL_ARRAY_BUFFER, m_bufferID);
+                glBufferData(GL_ARRAY_BUFFER, count * sizeof(GLfloat), data, GL_STATIC_DRAW);
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+            }
 
-		void Buffer::bind() const
-		{
-			glBindBuffer(GL_ARRAY_BUFFER, m_bufferID);
-		}
+            void Buffer::bind() const
+            {
+                glBindBuffer(GL_ARRAY_BUFFER, m_bufferID);
+            }
 
-		void Buffer::unbind() const
-		{
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
-		}
-	}
+            void Buffer::unbind() const
+            {
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+            }
+        }
+    }
 }
--- Sparky-core/src/graphics/buffers/vertexarray.cpp
+++ Sparky-core/src/graphics/buffers/vertexarray.cpp
@@ -1,42 +1,44 @@
 #include "vertexarray.h"
 
 namespace sparky {
-	namespace mock {
+    namespace graphics {
+        namespace buffers {
 
-		VertexArray::VertexArray()
-		{
-			glGenVertexArrays(1, &m_arrayID);
-		}
+            VertexArray::VertexArray()
+            {
+                glGenVertexArrays(1, &m_arrayID);
+            }
 
-		VertexArray::~VertexArray()
-		{
-			for (std::size_t i = 0; i < m_buffers.size(); i++)
-			{
-				delete m_buffers[i];
-			}
-		}
-		
-		void VertexArray::addBuffer(Buffer* buffer, GLuint index)
-		{
-			bind();
-			buffer->bind();
-			glEnableVertexAttribArray(index);
-			//glEnableVertexArrayAttrib(index);
+            VertexArray::~VertexArray()
+            {
+                for (std::size_t i = 0; i < m_buffers.size(); i++)
+                {
+                    delete m_buffers[i];
+                }
+            }
 
-			glVertexAttribPointer(index, buffer->getComponentCount(), GL_FLOAT, GL_FALSE, 0, 0);
+            void VertexArray::addBuffer(Buffer* buffer, GLuint index)
+            {
+                bind();
+                buffer->bind();
+                glEnableVertexAttribArray(index);
+                //glEnableVertexArrayAttrib(index);
 
-			buffer->unbind();
-			unbind();
-		}
-		
-		void VertexArray::bind() const
-		{
-			glBindVertexArray(m_arrayID);
-		}
+                glVertexAttribPointer(index, buffer->getComponentCount(), GL_FLOAT, GL_FALSE, 0, 0);
 
-		void VertexArray::unbind() const
-		{
-			glBindVertexArray(0);
-		}
-	}
+                buffer->unbind();
+                unbind();
+            }
+
+            void VertexArray::bind() const
+            {
+                glBindVertexArray(m_arrayID);
+            }
+
+            void VertexArray::unbind() const
+            {
+                glBindVertexArray(0);
+            }
+        }
+    }
 }
